83f58531262100704fc657254a6f5a4f8b56d944
pranavlonsane777 <pranavlonsane1999@gmail.com>
2026-06-12 18:18:34 +0530

    deleted src and files

--- src/org/devops/GitUtils.groovy
+++
@@ -1,9 +0,0 @@
-package org.devops
-
-class GitUtils implements Serializable {
-    def steps
-    GitUtils(steps) { this.steps = steps }
-    def cloneRepo(String repoURL, String branch){
-        sh "git clone -b ${branch} ${repoURL}"
-    }
-}
